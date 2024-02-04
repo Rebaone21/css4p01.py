72ec9eee0118bf223f2c61a3b769591ba2f76059
Rebaone21 <157544432+Rebaone21@users.noreply.github.com>
2024-02-04 19:43:26 +0200

    Add files via upload

---
+++ Movies dataset.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  4 19:04:48 2024
+
+@author: Rebao
+"""
+
+import pandas as pd
+
+file = pd.read_csv("C:/Users/Rebao/Movie data set quiz/movie_dataset (1).csv")
+
+print(file)
+
+print(file.describe())
